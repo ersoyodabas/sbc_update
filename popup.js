22226ed83fa1e27e697f527b475e0dbe713330b6
Ersoy Odabas <ersoyodabas@outlook.com>
2025-02-02 14:41:47 +0300

    next

--- popup.js
+++ popup.js
@@ -1,35 +1,44 @@
 document.getElementById("updateButton").addEventListener("click", () => {
+    debugger;
     const extensionId = chrome.runtime.id; // Eklentinin kimliği
-    const updateUrl = "https://ersoyodabas.github.io/sbc_update/updates.xml";
+    debugger;
+    const updateUrl = "https://ersoyodabas.github.io/testapp/github/updates.xml";
+    debugger;
   
     fetch(updateUrl)
       .then(response => response.text())
       .then(data => {
+        console.log(data,11);
+        debugger;
         const parser = new DOMParser();
         const xmlDoc = parser.parseFromString(data, "text/xml");
+        console.log(xmlDoc,15);
         const version = xmlDoc.getElementsByTagName("updatecheck")[0].getAttribute("version");
+        console.log(version,16);
         const currentVersion = chrome.runtime.getManifest().version;
-  
         if (version > currentVersion) {
+            debugger;
           // Yeni bir sürüm varsa eklentiyi güncelle
-          setTimeout(() => {
-            chrome.management.setEnabled(extensionId, false, () => {
-              chrome.management.setEnabled(extensionId, true, () => {
-                // Güncelleme tamamlandıktan sonra bir mesaj göster
-                chrome.notifications.create('update-complete', {
-                  type: 'basic',
-                  iconUrl: 'icon48.png',
-                  title: 'Güncelleme Tamamlandı',
-                  message: 'Eklenti başarıyla güncellendi.',
-                });
-              });
-            });
-          }, 100); // Popup penceresinin kapanmasını 100 ms geciktir
+             chrome.management.setEnabled(extensionId, false, () => {
+            //   chrome.management.setEnabled(extensionId, true, () => {
+            //     // Güncelleme tamamlandıktan sonra bir mesaj göster
+            //     chrome.notifications.create('update-complete', {
+            //       type: 'basic',
+            //       iconUrl: 'icon48.png',
+            //       title: 'Güncelleme Tamamlandı',
+            //       message: 'Eklenti başarıyla güncellendi.',
+            //     });
+            //   });
+             });
+            chrome.runtime.reload();
+
         } else {
           document.getElementById("message").textContent = "Eklenti zaten güncel.";
         }
       })
       .catch(error => {
+        alert();
+        debugger;
         console.error("Güncelleme kontrolü başarısız:", error);
         document.getElementById("message").textContent = "Güncelleme kontrolü başarısız.";
       });
